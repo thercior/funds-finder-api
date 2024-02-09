cff21131ec5cf3195a91a3ad0af66246278042f9
Thércio Rodrigues <thercior@gmail.com>
2024-02-09 20:55:59 -0300

    Adição do model FundsEstate ao admin

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import FundsEstate
+
+
+@admin.register(FundsEstate)
+class FundsEstateAdmin(admin.ModelAdmin):
+    
+    list_display = ('code', 'sector', 'dividend_yield_avg_12m', 'amount_assets', 'id')
 
 # Register your models here.
